68bd3397dbf4c3ce419e8f91640782d9eb28804e
ayush-chahar <chahar.ayush24@gmail.com>
2024-04-07 23:50:57 +0530

    Final

--- admin/src/Pages/Admin/Admin.jsx
+++ admin/src/Pages/Admin/Admin.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Admin.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
-import { Routes,Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import AddProduct from '../../components/AddProduct/AddProduct'
 import ListProduct from '../../components/ListProduct/ListProduct'
 const Admin = () => {
     return (
         <div className='admin'>
-            <Sidebar/>
+            <Sidebar />
             <Routes>
-                <Route path='/addproduct' element={<AddProduct/>}/>
-                <Route path='/listproduct' element={<ListProduct/>}/>
+                <Route path='/addproduct' element={<AddProduct />} />
+                <Route path='/listproduct' element={<ListProduct />} />
             </Routes>
         </div>
     )
--- backend/index.js
+++ backend/index.js
@@ -262,10 +262,10 @@ app.post('/removefromcart', fetchUser, async (req, res) => {
 })
 // creating endpoint to get cartdata
 
-app.post('/getcart', fetchUser, async (req, res) =>{
+app.post('/getcart', fetchUser, async (req, res) => {
     console.log("GetCart");
-let userData = await Users.findOne({_id:req.user.id}); 
-res.json(userData.cartData);
+    let userData = await Users.findOne({ _id: req.user.id });
+    res.json(userData.cartData);
 })
 // Error Handling
 server.on('error', (error) => {
